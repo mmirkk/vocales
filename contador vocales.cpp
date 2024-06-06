7bacf3e210765897c682a3e3d0b7e286cd6392a8
mirk <146762065+mmirkk@users.noreply.github.com>
2024-06-06 15:40:11 -0300

    Update contador vocales.cpp

--- contador vocales.cpp
+++ contador vocales.cpp
@@ -1,31 +1,30 @@
 #include<iostream>
 #include<string>
-#include <ctype.h>
+#include<ctype.h>
 
 using namespace std;
 
-int iterador(int letras, string cadena){
+int iterador(int letras, string texto){
 
     int contador = 0;
-    char ch;
+    char letra;
 
     for(int i = 0; i <= letras; i++){
-        ch = tolower(cadena[i]);
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-            contador += 1;
-        }
+        letra = tolower(texto[i]);
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+        contador ++;
     }
 
+}
     return contador;
-
 }
 
-int main(){
 
+int main(){
     int longitud;
     string cadena;
 
-    cout << "Ingrese una cadena de texto para contar sus vocales: ";
+    cout << "Ingrese un texto: ";
     cin >> cadena;
 
     longitud = cadena.size();
